c0401345eea1be842a00925fc8c3c12835ec5c0f
admin <admin@Venkat>
2017-05-04 14:11:43 +0530

    mvcDemo1

--- mvcDemo1/src/main/java/com/mvc/controller/PersonController.java
+++ mvcDemo1/src/main/java/com/mvc/controller/PersonController.java
@@ -1,26 +1,43 @@
 package com.mvc.controller;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import com.mvc.model.Person;
+import com.mvc.service.PersonService;
+
+
 
 @Controller
-
 public class PersonController {
-	@RequestMapping("/personForm")
-	public String getPersonForm(Model model)
-	{
-		model.addAttribute("personObj",new Person());
+	@Autowired
+	private PersonService personService;
+	
+	//personController.setPersonService(new PersonServiceImpl())
+	
+	
+	
+	//http://localhost:8080/mvcdemo/personForm
+    @RequestMapping("/personForm")
+	public String getPersonForm(Model model){
+		model.addAttribute("personObj",new Person()); // firstname,lastname,email and phone are null
 		return "personform";
 	}
-	    
-    public String getPersonDetails(@ModelAttribute(value="personObj")  Person person,Model model)
-    {
-    	model.addAttribute("personObj",person);
-    	return "persondetails";	    	
-	}
+    //personObj - to read the value from spring form fields
+    //person - to send the value to the persondetails.jsp
+   @RequestMapping("/processPersonDetails") // firstname,lastname,email and phone has some values
+    public String getPersonDetails(@ModelAttribute(value="personObj")  Person person,Model model){
+	   
+	   personService.savePerson(person);
+	   
+    	model.addAttribute("person",person);
+    	return "persondetails";
+    	
+    }
 }
 
 
--- mvcDemo1/src/main/webapp/WEB-INF/pages/personform.jsp
+++ mvcDemo1/src/main/webapp/WEB-INF/pages/personform.jsp
@@ -2,13 +2,15 @@
     pageEncoding="ISO-8859-1"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <%@ taglib uri="http://www.springframework.org/tags/form" prefix="form" %>
+<%@ taglib uri="http://www.springframework.org/tags" prefix="url"%>
 <html>
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
 		<title>Insert title here</title>
 	</head>
 	<body>
-		<form:form action="PersonController"  modelAttribute="personObj">
+		<url:url value="/processPersonDetails" var="url"></url:url>
+		<form:form action="${url }"  modelAttribute="personObj">
 			<table>
 				<tr>
 					<td><label for="firstname">Enter Firstname</label></td>
